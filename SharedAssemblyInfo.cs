4701e5f88ad5091065fb2194c7e4b9d608722d1d
aerik <asylvan@gmail.com>
2026-06-06 10:19:45 -0700

    bump rev after tls cert loading fix; 3.0.0.34

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -17,6 +17,6 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("3.0.0.33")]
-[assembly: AssemblyFileVersion("3.0.0.33")]
-[assembly: AssemblyInformationalVersion("3.0.0.33")]
+[assembly: AssemblyVersion("3.0.0.34")]
+[assembly: AssemblyFileVersion("3.0.0.34")]
+[assembly: AssemblyInformationalVersion("3.0.0.34")]
